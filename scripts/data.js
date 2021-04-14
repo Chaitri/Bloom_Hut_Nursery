495640cf012a3aed4163d66a2ff2b7e94e07289c
Chaitri <chaitri.s.mohan@gmail.com>
2021-04-14 22:41:07 +0530

    Add files

---
+++ scripts/data.js
@@ -0,0 +1,185 @@
+let plants = [
+    {
+        src: "assets/media/begonia_cheimantha.jfif",
+        alt: "begonia_cheimantha",
+        name: "Begonia Cheimantha",
+        price: '₹ 80'
+    },
+    {
+        src: "assets/media/Aglaonema_silver_king.jfif",
+        alt: "Aglaonema silver king",
+        name: "Aglaonema silver king",
+        price: '₹ 90'
+    },
+    {
+        src: "assets/media/belladonna_lily.jfif",
+        alt: "Belladonna lily",
+        name: "Belladonna lily",
+        price: '₹ 60'
+    },
+    {
+        src: "assets/media/calathea_bella.jfif",
+        alt: "Calathea bella",
+        name: "Calathea bella",
+        price: '₹ 120'
+    },
+    {
+        src: "assets/media/Calathea_Zebrina.jfif",
+        alt: "Calathea Zebrina",
+        name: "Calathea Zebrina",
+        price: '₹ 90'
+    },
+    {
+        src: "assets/media/Costus_speciosus.jfif",
+        alt: "Costus Speciosus",
+        name: "Costus Speciosus",
+        price: '₹ 110'
+    },
+    {
+        src: "assets/media/daylily.jfif",
+        alt: "Daylily",
+        name: "Daylily",
+        price: '₹ 130'
+    },
+    {
+        src: "assets/media/peace_lily.jfif",
+        alt: "Peace Lily",
+        name: "Peace Lily",
+        price: '₹ 90'
+    },
+    {
+        src: "assets/media/peperomia_varities.jfif",
+        alt: "Peperomia Varities",
+        name: "Peperomia Varities",
+        price: '₹ 70'
+    },
+    {
+        src: "assets/media/rain_lily.jfif",
+        alt: "Rain Lily",
+        name: "Rain Lily",
+        price: '₹ 90'
+    },
+    {
+        src: "assets/media/schefflera_arboricola.jfif",
+        alt: "Schefflera Arboricola",
+        name: "Schefflera Arboricola",
+        price: '₹ 100'
+    },
+    {
+        src: "assets/media/split-leaf-philodendron.jfif",
+        alt: "Split Leaf Philodendron",
+        name: "Split Leaf Philodendron",
+        price: '₹ 90'
+    },
+    {
+        src: "assets/media/Water_Pennywort.jfif",
+        alt: "Water Pennywort",
+        name: "Water Pennywort",
+        price: '₹ 50'
+    },
+    {
+        src: "assets/media/white_indian_lotus.jfif",
+        alt: "White Indian Lotus",
+        name: "White Indian Lotus",
+        price: '₹ 90'
+    },
+    {
+        src: "assets/media/yellow_rain_lily.jfif",
+        alt: "Yellow Rain Lily",
+        name: "Yellow Rain Lily",
+        price: '₹ 120'
+    },
+];
+
+let planters = [
+    {
+        src: "assets/media/ceramic_pot_3.jfif",
+        alt: "Teal Hanging Pot",
+        name: "Teal Hanging Pot",
+        price: '₹ 120'
+    },
+    {
+        src: "assets/media/ceramic_pot.jfif",
+        alt: "Set of Ceramic Pots",
+        name: "Set of Ceramic Pots",
+        price: '₹ 350'
+    },
+    {
+        src: "assets/media/ceramic_pot2.jfif",
+        alt: "Swirls Ceramic Pot",
+        name: "Swirls Ceramic Pot",
+        price: '₹ 150'
+    },
+    {
+        src: "assets/media/clay_pot.jfif",
+        alt: "Set of Clay Pots",
+        name: "Set of Clay Pots",
+        price: '₹ 180'
+    },
+    {
+        src: "assets/media/clay_pot2.jfif",
+        alt: "Clay Pot",
+        name: "Clay Pot",
+        price: '₹ 80'
+    },
+    {
+        src: "assets/media/planter2.jfif",
+        alt: "Simple Planter",
+        name: "Simple Planter",
+        price: '₹ 180'
+    },
+    {
+        src: "assets/media/red_planter.jfif",
+        alt: "Red Planter",
+        name: "Red Planter",
+        price: '₹ 280'
+    },
+    {
+        src: "assets/media/clay_hanging_pot.jfif",
+        alt: "Clay Hanging Pot",
+        name: "Clay Hanging Pot",
+        price: '₹ 200'
+    },
+    {
+        src: "assets/media/stone_pot_2.jfif",
+        alt: "Small Stone Planter",
+        name: "Small Stone Planter",
+        price: '₹ 150'
+    },
+    {
+        src: "assets/media/stone_pot_3.jfif",
+        alt: "Large Stone Planter",
+        name: "Large Stone Planter",
+        price: '₹ 350'
+    },
+    {
+        src: "assets/media/stone_pot.jfif",
+        alt: "Simple Stone Planter",
+        name: "Simple Stone Planter",
+        price: '₹ 250'
+    },
+    {
+        src: "assets/media/succulent_pot.jfif",
+        alt: "Set of Succulent Pots",
+        name: "Set of Succulent Pots",
+        price: '₹ 550'
+    },
+    {
+        src: "assets/media/teal_planter.jfif",
+        alt: "Teal Planter",
+        name: "Teal Planter",
+        price: '₹ 250'
+    },
+    {
+        src: "assets/media/terracotta_pot.jfif",
+        alt: "Terracotta Planter",
+        name: "Terracotta Planter",
+        price: '₹ 350'
+    },
+    {
+        src: "assets/media/white_decor_pot.jfif",
+        alt: "White Decor Pot",
+        name: "White Decor Pot",
+        price: '₹ 190'
+    },
+]
